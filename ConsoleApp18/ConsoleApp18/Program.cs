743110f7bb14d9e67b279b654224bde75a51e838
Сом <117897751+ILYA222213@users.noreply.github.com>
2023-09-14 12:15:40 +0300

    Add files via upload

---
+++ ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int result = 0;
+int sign1 = Math.Sign(a);
+int singn2 = Math.Sign(b);
+
+a = Math.Abs(a);
+b = Math.Abs(b);
+for (var i = 0; i < a; i++) result += b;
+if (sign1 == singn2)
+{
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine(-result);
+}
